a2161f77014b922dfdde3de434ff3f6446e549d0
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-22 22:21:29 +0000

    Testing Wed Jan 22 2025 22:21:29 GMT+0000 (Coordinated Universal Time)

--- chapter1/de04/student/DebugOne4.java
+++ chapter1/de04/student/DebugOne4.java
@@ -2,7 +2,7 @@ public class DebugOne4
 {
    public static void main(String[] args)
    {
-      System.out.println("This output is on the same line as the last one");
+      System.out.println("This output is on the same line as the last one.");
       System.out.println("But this is on a new line.");
    }
 }
