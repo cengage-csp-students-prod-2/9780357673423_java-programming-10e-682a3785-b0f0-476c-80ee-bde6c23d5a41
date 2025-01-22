0fba6662b608c5daabd18b3d61169e8bf33435a1
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-22 22:01:22 +0000

    Testing Wed Jan 22 2025 22:01:22 GMT+0000 (Coordinated Universal Time)

--- chapter1/de02/student/DebugOne2.java
+++ chapter1/de02/student/DebugOne2.java
@@ -1,12 +1,10 @@
-public class DebugOne2
-{
+public class DebugOne2 {
    /* This program displays some output*/
-   public static void Main(String[] args)
-   {
-      System.outprintln("Java programming is fun.");
-      System.outprintln("Getting a program to work);
-      System.outprintln(can be a challenge,");
-      System.outprintln("but when everything works correctly,");
-      System.outprintln(it's very satisfying");
+   public static void main(String[] args){
+      System.out.println("Java programming is fun.");
+      System.out.println("Getting a program to work");
+      System.out.println("can be a challenge,");
+      System.out.println("but when everything works correctly,");
+      System.out.println("it's very satisfying");
    }
 }
