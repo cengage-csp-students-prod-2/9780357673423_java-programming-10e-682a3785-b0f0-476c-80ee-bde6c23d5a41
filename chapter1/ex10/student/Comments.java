c98f6d3ee2874e08c821fc47d7ed57ecc1de9a09
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-22 21:26:42 +0000

    Testing Wed Jan 22 2025 21:26:42 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex10/student/Comments.java
+++ chapter1/ex10/student/Comments.java
@@ -1,4 +1,6 @@
 class Comments {
+
+    //This is a comment
     public static void main(String []args) {
         System.out.println(" Program comments are nonexecuting statements");
         System.out.println("you add to a file for documentation.");
