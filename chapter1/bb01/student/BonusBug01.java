d4ffe57f5126cd38f367b12128ed747a0aa9b55a
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-22 22:26:42 +0000

    Testing Wed Jan 22 2025 22:26:42 GMT+0000 (Coordinated Universal Time)

--- chapter1/bb01/student/BonusBug01.java
+++ chapter1/bb01/student/BonusBug01.java
@@ -1,12 +1,12 @@
 /* File Name: BonusBug01.java
 This is a debugging activity to learn the syntax of Java.
 In this exercise, a welcome message for the guessing game is displayed as a console output.
-//
+*/
 
 public class BonusBug01
 {
-   public static void main(Strings[] args)
+   public static void main(String []args)
    {
-system.Out.println("Welcome to the Guessing Game!!!")
+  System.out.println("Welcome to the Guessing Game!!!");
   }
 }
