1577ac66c893f533aefe89a3455f86635f8ca025
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-22 21:43:26 +0000

    Testing Wed Jan 22 2025 21:43:25 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex11/student/BurmaShave.java
+++ chapter1/ex11/student/BurmaShave.java
@@ -1,1 +1,10 @@
-// Write your code here
+public class BurmaShave {
+    public static void main(String []args) {
+        System.out.println("Shave the modern way");
+        System.out.println("No brush");
+        System.out.println("No lather");
+        System.out.println("No rub-in");
+        System.out.println("Big tube 35 cents drug stores");
+        System.out.println("Burma Shave");
+    }
+}
