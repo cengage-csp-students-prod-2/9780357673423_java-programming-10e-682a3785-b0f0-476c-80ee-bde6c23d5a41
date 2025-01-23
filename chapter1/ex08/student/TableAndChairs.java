ea416f293a63a9083b20853282f646a92402fcee
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-23 23:14:50 +0000

    Testing Thu Jan 23 2025 23:14:50 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex08/student/TableAndChairs.java
+++ chapter1/ex08/student/TableAndChairs.java
@@ -1,10 +1,10 @@
 class TableAndChairs {
     public static void main(String[] args) {
-        System.out.println(" x                      x");
-        System.out.println("x                      x");
-        System.out.println("x      xxxxxxxxxx      x");
-        System.out.println("xxxxx  x        x  xxxxx");
-        System.out.println("x   x  x        x  x   x");
-        System.out.println("x   x  x        x  x   x");
+        System.out.println(" X                      X");
+        System.out.println("X                      X");
+        System.out.println("X      XXXXXXXXXX      X");
+        System.out.println("XXXXX  X        X  XXXXX");
+        System.out.println("X   X  X        X  X   X");
+        System.out.println("X   X  X        X  X   X");
     }
 }
