b23bd77ca2c0ff0f6feb386707e2cb9456c8e387
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-28 17:14:26 +0000

    Testing Tue Jan 28 2025 17:14:26 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex05/student/MileConversions.java
+++ chapter2/ex05/student/MileConversions.java
@@ -1,1 +1,12 @@
-// Write your code here
+public class MileConversions
+{
+    public static void main(String[] args)
+    {
+        int INCHES_IN_MILE = 63360;
+        int FEET_IN_MILE = 5280;
+        int YARDS_IN_MILE = 1760;
+        int MILES = 3;
+        JOptionPane.showMessageDialog(null, "There are " + INCHES_IN_MILE, 
+        "inches," + FEET_IN_MILE, " feet, and" + YARDS_IN_MILE, " yards in a mile");
+    }
+}
