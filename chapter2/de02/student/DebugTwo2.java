b5ff118cf7c0d156537eab9cc31e35edd82be02f
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-28 18:34:34 +0000

    Testing Tue Jan 28 2025 18:34:34 GMT+0000 (Coordinated Universal Time)

--- chapter2/de02/student/DebugTwo2.java
+++ chapter2/de02/student/DebugTwo2.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class DebugTwo2
 {
-   public static void main(String args[])
+   public static void main(String[] args)
    {
-      int a, b;
-      Scanner input = new Scanner(System.in);
+      int a;
+      int b;
+      Scanner inputDevice = new Scanner(System.in);
       System.out.print("Enter an integer >> ");
-      a = nextInt();
+      a = inputDevice.nextInt();
       System.out.print("Enter another integer >> ");
-      b = nextInt();
-      System.out.print1n("The sum is " + (a + b));
+      b = inputDevice.nextInt();
+      System.out.println("The sum is " + (a + b));
       System.out.println("The difference is " + (a - b));
       System.out.println("The product is " + (a * b));
    }
