4b4fe63970ff4a53584a21ee26f0b9911c0e4988
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-29 15:11:10 +0000

    Testing Wed Jan 29 2025 15:11:10 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/InchesToFeet.java
+++ chapter2/ex06/student/InchesToFeet.java
@@ -1,1 +1,13 @@
-// Write your code here
+public class InchesToFeet {
+    
+    public static final int inches = 86;
+
+
+    public static void main(String[] args) {
+        int feet = inches / 12;
+        int remaininginches = inches % 12;
+
+        System.out.println(inches + " inches is " + feet + " feet and "
+            + remaininginches + " inches");
+    }
+}
