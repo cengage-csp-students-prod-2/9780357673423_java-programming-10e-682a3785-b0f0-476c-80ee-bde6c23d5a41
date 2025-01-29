2cb4512777a4db12e980104e008ffecc5c2af1c2
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-29 14:45:18 +0000

    Testing Wed Jan 29 2025 14:45:18 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex05/student/MileConversions.java
+++ chapter2/ex05/student/MileConversions.java
@@ -1,10 +1,19 @@
 public class MileConversions {
-    double INCHES_IN_MILE = 63360;
-    double FEET_IN_MILE = 5280;
-    double YARDS_IN_MILE = 1760;
-    double miles = 2;
+    public static final int INCHES_IN_MILE = 63360;
+    public static final int FEET_IN_MILE = 5280;
+    public static final int YARDS_IN_MILE = 1760;
     
     public static void main(String[] args){
-        
+        double miles = 2;
+
+
+        double inches = miles * INCHES_IN_MILE;
+        double feet = miles * FEET_IN_MILE;
+        double yards = miles * YARDS_IN_MILE;
+
+        System.out.println(miles + " miles in equivalent to:");
+        System.out.println(inches + " inches");
+        System.out.println(feet + " feet");
+        System.out.println(yards + " yards");
     }
 }
