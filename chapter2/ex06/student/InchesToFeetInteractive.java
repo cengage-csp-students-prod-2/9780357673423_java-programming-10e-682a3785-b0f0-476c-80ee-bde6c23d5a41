a5be2f042eb6303db52348063408e9c7ca34152e
DJohnson77 <djohnson0936@kctcs.edu>
2025-01-29 15:38:56 +0000

    Testing Wed Jan 29 2025 15:38:56 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/InchesToFeetInteractive.java
+++ chapter2/ex06/student/InchesToFeetInteractive.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class InchesToFeetInteractive {
     public static void main(String[] args) {
         Scanner inputDevice = new Scanner(System.in);
-        final double inches;
+        double inches;
         
         System.out.print("Enter a number of inches >> ");
         inches = inputDevice.nextDouble();
 
-        final double feet = inches / 12;
-        final double remainingInches = inches % 12;
+        int feet = (int) (inches / 12);
+        int remainingInches = (int) Math.round(inches % 12);
 
         System.out.print(inches + " inches converted into feet is "
             + feet + " and " + remainingInches + " inches");
