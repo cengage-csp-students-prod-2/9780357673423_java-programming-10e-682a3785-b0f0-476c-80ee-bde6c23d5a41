c39ec4e50f6af5fbf73e853e9b59df863c6df527
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-12 15:57:40 +0000

    Testing Wed Feb 12 2025 15:57:40 GMT+0000 (Coordinated Universal Time)

--- chapter3/de02/student/DebugThree2.java
+++ chapter3/de02/student/DebugThree2.java
@@ -12,29 +12,29 @@ public class DebugThree2
       b = input.nextInt();
       System.out.print("Enter a third integer >> ");
       c = input.nextInt();
-   
+
       add(a, b, c);
       subtract(a, b, c);
-   }
-   
-   public static void add(int a, int b, int c) {
 
-      System.out.println("The sum of " + a +
-         " and " + b + " is " + (a + b));
-      System.out.println("The sum of " + b +
-         " and " + c + " is " + (b + c));
-      System.out.println("The sum of " + a +
-         " and " + c + " is " + (a + c));
-      }
-  
-   public static void subtract(int a, int b, int c){
-
-      System.out.println("The difference between " +
-         a + " and " + b + " is "  +  (a - b));
-      System.out.println("The difference between " +
-         b + " and " + c + " is "  +  (b - c));
-      System.out.println("The difference between " +
-         a + " and " + c + " is "  +  (a - c));
    }
 
+   public static void add(int a, int b, int c)
+   {
+   System.out.println("The sum of " + a + " and " + b + " is "
+         + (a + b));
+   System.out.println("The sum of " + b + " and " + c + " is "
+         + (b + c));
+   System.out.println("The sum of " + a + " and " + c + " is "
+          + (a + c));
+   }
+
+   public static void subtract(int a, int b, int c)
+   {
+      System.out.println("The difference of " + a + " and " + b + " is "
+         + (a - b));
+      System.out.println("The difference of " + b + " and " + c + " is "
+         + (b - c));
+      System.out.println("The difference of " + a + " and " + c + " is "
+         + (a - c));
+   }
 }
