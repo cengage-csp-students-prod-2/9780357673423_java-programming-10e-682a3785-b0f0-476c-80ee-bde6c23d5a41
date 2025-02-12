dbc7221a0d7082661dbda9afc9f550ef196b9b92
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-12 14:45:08 +0000

    Testing Wed Feb 12 2025 14:45:08 GMT+0000 (Coordinated Universal Time)

--- chapter3/de01/student/DebugThree1.java
+++ chapter3/de01/student/DebugThree1.java
@@ -9,17 +9,19 @@ public class DebugThree1
       double check2;
       Scanner input = new Scanner(System.in);
       System.out.print("Enter the amount of your check >> ");
-      check1 = input.nextInt();
-      System.out.print("Enter the amount of your friend's check >> ")
-      check2 = inputnextInt();
-      calcTip(check1);
-      calcTip(check2);
+      check1 = input.nextDouble();
+
+      System.out.print("Enter the amount of your friend's check >> ");
+      check2 = input.nextDouble();
+
+      calculateTip(check1);
+      calculateTip(check2);
     }
-    public static void calcTip()
+    public static double calculateTip(double bill)
     {
-       final doubel RATE = 0.15;
-       double tip;
-       tip = bill / RATE;
+       final double RATE = 0.15;
+       double tip = bill * RATE;
        System.out.println("The tip should be at least $" + tip);
+       return tip;
     }
 }
