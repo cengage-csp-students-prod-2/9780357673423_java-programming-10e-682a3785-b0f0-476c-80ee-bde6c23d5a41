0f8a41dd87c2aef65f4b93506cb924040e9cad73
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-12 15:07:19 +0000

    Testing Wed Feb 12 2025 15:07:19 GMT+0000 (Coordinated Universal Time)

--- chapter3/de02/student/DebugThree2.java
+++ chapter3/de02/student/DebugThree2.java
@@ -2,31 +2,39 @@
 import java.util.Scanner;
 public class DebugThree2
 {
-   public static void main(String args[])
-   {
+   public static void main(String[] args) {
+   
       int a, b, c;
       Scanner input = new Scanner(System.in);
       System.out.print("Enter an integer >> ");
       a = input.nextInt();
       System.out.print("Enter a second integer >> ");
-      a = input.nextInt();
+      b = input.nextInt();
       System.out.print("Enter a third integer >> ");
-      a = input.nextInt();
-      add(a, b);
-      add(b, c);
-      add(a, c);
-      subtact(a, b);
-      subtact(b, c);
-      subtact(a, c);  
+      c = input.nextInt();
+   
+      add(a, b, c);
+      subtract(a, b, c);
    }
-   public static int add(int a, int  b)
-   {
+   
+   public static void add(int a, int b, int c) {
+
       System.out.println("The sum of " + a +
-         " and " + b + " is " + a + b);
-   }
-   public static void subtract(int a, int b)
-   {
+         " and " + b + " is " + (a + b));
+      System.out.println("The sum of " + b +
+         " and " + c + " is " + (b + c));
+      System.out.println("The sum of " + a +
+         " and " + c + " is " + (a + c));
+      }
+  
+   public static void subtract(int a, int b, int c){
+
       System.out.println("The difference between " +
-         a + " and  + b + " is  +  (a - b));
+         a + " and " + b + " is "  +  (a - b));
+      System.out.println("The difference between " +
+         b + " and " + c + " is "  +  (b - c));
+      System.out.println("The difference between " +
+         a + " and " + c + " is "  +  (a - c));
    }
+
 }
