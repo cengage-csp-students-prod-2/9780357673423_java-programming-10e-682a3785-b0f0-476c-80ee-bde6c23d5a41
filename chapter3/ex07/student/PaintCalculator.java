b7b8c97a0dd5cf652e2716a46bff192374d9f564
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-12 21:32:22 +0000

    Testing Wed Feb 12 2025 21:32:22 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex07/student/PaintCalculator.java
+++ chapter3/ex07/student/PaintCalculator.java
@@ -1,1 +1,39 @@
-// Write your code here
+import java.util.Scanner;
+public class PaintCalculator
+{
+    public static void main(String[] args)
+    {
+        double length;
+        double height;
+        double width;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the room's length >> ");
+        length = input.nextDouble();
+        System.out.print("Enter the room's width >> ");
+        width = input.nextDouble();
+        System.out.print("Enter the room's height >> ");
+        height = input.nextDouble();
+
+        
+        double price = computeArea(length, height, width);
+
+        System.out.print("The price to paint the room is $" + price);
+
+        input.close();
+    }
+
+    public static double computeGallons(double wallArea){
+        return wallArea / 350;
+    }
+    
+    public static double computeArea(double l, double h, double w)
+    {
+        double wallArea = (l * w * h);
+        double gallonsNeeded = computeGallons(wallArea);
+        System.out.println("You will need " + gallonsNeeded + " gallons");
+        double price = gallonsNeeded * 32;
+        return price;
+    }
+
+
+}
