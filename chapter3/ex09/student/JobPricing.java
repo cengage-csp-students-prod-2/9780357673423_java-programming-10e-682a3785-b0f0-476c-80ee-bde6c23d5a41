2070fa2ac6d5f07d65c1a72853d46d1df4f9f2a9
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-14 13:47:03 +0000

    Testing Fri Feb 14 2025 13:47:03 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex09/student/JobPricing.java
+++ chapter3/ex09/student/JobPricing.java
@@ -1,1 +1,33 @@
-// Write your code here
+import java.util.Scanner;
+
+public class JobPricing
+{
+public static void main(String[] args)
+{
+    String job_desc;
+    double hours_worked;
+    double material_cost;
+    double hours_traveled;
+
+    Scanner input = new Scanner(System.in);
+    System.out.print(" Enter job description >> ");
+    job_desc = input.nextLine();
+    System.out.print("Enter cost of materials >> ");
+    material_cost = input.nextDouble();
+    System.out.print("Enter hours on the job work >> ");
+    hours_worked = input.nextDouble();
+    System.out.print("Enter hours traveling >> ");
+    hours_traveled = input.nextDouble();
+
+    double estimated_price = computePrice(hours_worked, material_cost,
+    hours_traveled);
+
+    System.out.print("The price for " + job_desc + " is $" + estimated_price);
+
+    }
+    public static double computePrice(double hw, double mc, double ht)
+    {
+
+    return (mc + (hw * 35) + (ht * 12));
+    }
+}
