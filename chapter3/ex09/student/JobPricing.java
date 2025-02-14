2474af89ec8e90bb026e5e288063df95f51e147a
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-14 14:02:58 +0000

    Testing Fri Feb 14 2025 14:02:58 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex09/student/JobPricing.java
+++ chapter3/ex09/student/JobPricing.java
@@ -28,7 +28,7 @@ public static void main(String[] args)
     }
     public static double computePrice(double hw, double mc, double ht)
     {
-
-    return (mc + (hw * 35) + (ht * 12));
+        double job_estimate;
+    return job_estimate = (mc + (hw * 35) + (ht * 12));
     }
 }
