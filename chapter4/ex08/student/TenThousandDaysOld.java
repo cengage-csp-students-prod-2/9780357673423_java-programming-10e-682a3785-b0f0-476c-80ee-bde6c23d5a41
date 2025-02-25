ee5b2725af2cd5b0caa569c0433464e968cc134c
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-25 14:52:20 +0000

    Testing Tue Feb 25 2025 14:52:20 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex08/student/TenThousandDaysOld.java
+++ chapter4/ex08/student/TenThousandDaysOld.java
@@ -1,1 +1,26 @@
-// Write your code here
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TenThousandDaysOld
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int birthMonth;
+        int birthDay;
+        int birthYear;
+
+        System.out.print("Enter your birth month as an integer >> ");
+        birthMonth = scanner.nextInt();
+        System.out.print("Enter your birth day as an integer >> ");
+        birthDay = scanner.nextInt();
+        System.out.print("Enter your birth year as a four-digit integer >> ");
+        birthYear = scanner.nextInt();
+       
+        LocalDate birthDate = LocalDate.of(birthYear, birthMonth, birthDay);
+        LocalDate daysOld = birthDate.plusDays(10000);
+       
+        System.out.println("You are 10,000 days old on " + daysOld);
+    }
+}
