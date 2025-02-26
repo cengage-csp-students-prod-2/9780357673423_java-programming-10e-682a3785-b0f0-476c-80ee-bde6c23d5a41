e9b4043005904b2b46c67067869f3f94465b7d16
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-26 01:21:38 +0000

    Testing Wed Feb 26 2025 01:21:38 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex04/student/QuartsToGallons.java
+++ chapter2/ex04/student/QuartsToGallons.java
@@ -1,1 +1,17 @@
-// Write your code here
+public class QuartsToGallons
+{
+    public static void main(String[] args)
+    {
+        int QUARTS_IN_GALLON = 4;
+        int quartsNeeded = 18;
+        int divide;
+        int remainder;
+        
+        divide = quartsNeeded/QUARTS_IN_GALLON;
+        remainder = quartsNeeded%QUARTS_IN_GALLON;
+
+        System.out.print("A job that needs " + quartsNeeded + " quarts requires " + divide +
+        " gallons plus " + remainder + " quarts");
+        
+    }
+}
--- chapter2/ex04/student/QuartsToGallonsInteractive.java
+++ chapter2/ex04/student/QuartsToGallonsInteractive.java
@@ -1,1 +1,22 @@
-// Write your code here
+import java.util.Scanner;
+public class QuartsToGallonsInteractive
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int QUARTS_IN_GALLON = 4;
+        int quartsNeeded;
+        int divide;
+        int remainder;
+        
+        System.out.print("Enter quarts needed >> ");
+        quartsNeeded = input.nextInt();
+
+        divide = quartsNeeded/QUARTS_IN_GALLON;
+        remainder = quartsNeeded%QUARTS_IN_GALLON;
+
+        System.out.print("A job that needs " + quartsNeeded + " quarts requires " + divide +
+        " gallons plus " + remainder + " quarts");
+        
+    }
+}
