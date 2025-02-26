a2b689a9ca678b1ecb9aa6759fd645fab091461b
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-26 01:41:17 +0000

    Testing Wed Feb 26 2025 01:41:17 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex07/student/Eggs.java
+++ chapter2/ex07/student/Eggs.java
@@ -1,1 +1,23 @@
-// Write your code here
+import java.util.Scanner;
+
+public class Eggs
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int eggsReq;
+        int dozensOrdered;
+        int looseEggs;
+        double finalCost;
+
+        System.out.print("Enter amount of eggs >> ");
+        eggsReq = input.nextInt();
+
+        dozensOrdered = (eggsReq / 12);
+        looseEggs = (eggsReq % 12);
+        finalCost = (dozensOrdered * 3.25) + (looseEggs * .45);
+        
+        System.out.print("You ordered " + eggsReq + " eggs. That's " + dozensOrdered + " dozen at $3.25 per dozen and "
+        + looseEggs + " loose eggs at 45 cents each for a total of $" + finalCost);
+    }
+}
