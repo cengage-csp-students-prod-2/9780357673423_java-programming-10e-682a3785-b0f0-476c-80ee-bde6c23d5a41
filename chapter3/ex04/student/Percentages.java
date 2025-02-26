632c8a229e4b4f2117ce5762856aead4e1d7ae41
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-26 21:21:12 +0000

    Testing Wed Feb 26 2025 21:21:12 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex04/student/Percentages.java
+++ chapter3/ex04/student/Percentages.java
@@ -1,1 +1,25 @@
-// Write your code here
+import java.util.Scanner;
+
+public class Percentages{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        double numberOne;
+        double numberTwo;
+
+        System.out.print("Enter a double >> ");
+        numberOne = input.nextDouble();
+        System.out.print("Enter another double >> ");
+        numberTwo = input.nextDouble();
+        System.out.println(numberOne + " is " + computePercent(numberOne, numberTwo) + 
+        " percent of " + numberTwo);
+        System.out.println(numberTwo + " is " + computePercent(numberTwo, numberOne) + 
+        " percent of " + numberOne);
+
+    }
+
+    public static double computePercent(double one, double two){
+        return ((one / two) * 100);
+    }
+}
