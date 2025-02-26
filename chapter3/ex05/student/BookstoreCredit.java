3b2b480903d71e39e0f3a8b3f87ff6670c9cb91a
DJohnson77 <djohnson0936@kctcs.edu>
2025-02-26 21:47:02 +0000

    Testing Wed Feb 26 2025 21:47:02 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex05/student/BookstoreCredit.java
+++ chapter3/ex05/student/BookstoreCredit.java
@@ -1,1 +1,25 @@
-// Write your code here
+import java.util.Scanner;
+
+public class BookstoreCredit{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        String name;
+        double gpa;
+        double credit;
+
+        System.out.print("Enter your name >> ");
+        name = input.nextLine();
+
+        System.out.print("Enter your gpa >> ");
+        gpa = input.nextDouble();
+        credit = computeDiscount(gpa);
+        System.out.println(name + ", your GPA is " + gpa + ", so your credit is $"
+        + credit);
+    }
+        public static double computeDiscount(double gpa){
+            return (gpa * 10);
+        }
+    
+}
