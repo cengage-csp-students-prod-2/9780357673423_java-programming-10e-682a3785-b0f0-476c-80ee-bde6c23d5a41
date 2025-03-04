ce8721bd4590932c969a3a6af3e30a8ba0680942
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-04 00:10:38 +0000

    Testing Tue Mar 04 2025 00:10:38 GMT+0000 (Coordinated Universal Time)

--- chapter5/de01/student/DebugFive1.java
+++ chapter5/de01/student/DebugFive1.java
@@ -1,4 +1,4 @@
-/ DebugFive1
+// DebugFive1
 // Adds your lunch bill
 // Burger and hot dog are $2.59
 // Grilled cheese and fish are $1.99
@@ -13,18 +13,21 @@ public class DebugFive1
       final double MED_PRICE = 1.99;
       final double LOW_PRICE = 0.89;
       int usersChoice;
-      double bill;
+      double bill = 0.0;
+
       System.out.println("Order please  1 - Burger  2 - Hotdog");
       System.out.print("3 - Grilled cheese   4 - Fish sandwich >> ");
       usersChoice = kb.nextInt();
-      if(usersChoice == 1 && usersChoice == 2)
+
+      if(usersChoice == 1 || usersChoice == 2)
          bill = bill + HIGH_PRICE;
       else
          bill = bill + MED_PRICE;
+
       System.out.print("Fries with that?  1 - Yes  2 - No >> ");
       usersChoice = kb.nextInt();
-      if(usersChoice = 1)
+      if(usersChoice == 1)
          bill = bill + LOW_PRICE;
-      System.out.println(Total bill is " + bill);
+      System.out.println("Total bill is " + bill);
    }
 }
