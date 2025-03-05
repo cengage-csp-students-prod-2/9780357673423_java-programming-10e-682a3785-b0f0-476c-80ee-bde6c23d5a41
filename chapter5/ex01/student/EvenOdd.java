c0a594d172ec18ff49b5b4bdb61bf6abf190a3ee
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-05 14:30:40 +0000

    Testing Wed Mar 05 2025 14:30:40 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex01/student/EvenOdd.java
+++ chapter5/ex01/student/EvenOdd.java
@@ -1,1 +1,25 @@
-// Write your code here
+import java.util.Scanner;
+
+public class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Enter a number >> ");
+        number = input.nextInt();
+
+        if(isEven(number)){
+            System.out.print(number + " is even");
+        } else {
+            System.out.print(number + " is odd");
+        }
+        input.close();
+
+    }
+
+        public static boolean isEven(int num){
+            return num % 2 == 0;
+        }
+}
