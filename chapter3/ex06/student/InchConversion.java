0ae68a818cfd4eb31ef4c11e3f33b24694566032
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-05 14:48:56 +0000

    Testing Wed Mar 05 2025 14:48:56 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex06/student/InchConversion.java
+++ chapter3/ex06/student/InchConversion.java
@@ -1,1 +1,27 @@
-// Write your code here
+import java.util.Scanner;
+
+public class InchConversion
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        double inches;
+        double feet;
+        double yards;
+        
+        System.out.print("Enter inches >> ");
+        inches = input.nextDouble();
+        System.out.println(inches + " inches is " + convertToFeet(inches) + " feet");
+        System.out.println(inches + " inches is " + convertToYards(inches) + " yards");
+    }
+        public static double convertToFeet(double inches)
+        {
+            return (inches / 12);
+        }
+
+        public static double convertToYards(double inches)
+        {
+            return (inches / 12) / 3;
+        }
+}
