53d875c4a6f0d0f273e71753ef05ec29d5f889e2
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-07 01:08:00 +0000

    Testing Fri Mar 07 2025 01:08:00 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex10/student/MinutesConversion.java
+++ chapter2/ex10/student/MinutesConversion.java
@@ -1,1 +1,17 @@
-// Write your code here
+import java.util.Scanner;
+
+public class MinutesConversion
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        int minutes;
+        double hours, days;
+
+        System.out.print("Enter minutes >> ");
+        minutes = input.nextInt();
+        hours = (minutes / 60);
+        days = ((minutes / 60) / 24);
+        System.out.print(minutes + " minutes is " + hours + " hours or " + days + " days.");
+    }
+}
