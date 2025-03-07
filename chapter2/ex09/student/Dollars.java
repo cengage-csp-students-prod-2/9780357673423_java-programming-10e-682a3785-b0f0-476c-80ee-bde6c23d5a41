3b2ccec6e26a07128581d3ecd784e9caf57f03cc
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-07 00:33:17 +0000

    Testing Fri Mar 07 2025 00:33:17 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex09/student/Dollars.java
+++ chapter2/ex09/student/Dollars.java
@@ -1,1 +1,26 @@
-// Write your code here
+import java.util.Scanner;
+
+public class Dollars
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int dollars, twenties, tens, fives, ones, rem;
+
+
+        System.out.print("Enter a number of dollars ");
+        dollars = input.nextInt();
+        twenties = (dollars / 20);
+        rem = dollars % 20;
+        tens = (rem / 10);
+        rem = rem % 10;
+        fives = (rem / 5);
+        rem = rem % 5;
+        ones = rem;
+
+        System.out.println("$" + dollars + " converted is " + twenties + " $20s, " + tens + " $10s, " + 
+            fives + " $5s, and " + ones + " $1s");
+        
+    }
+}
