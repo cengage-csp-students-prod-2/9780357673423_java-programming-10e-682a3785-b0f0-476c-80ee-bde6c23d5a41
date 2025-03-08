84f858db6a8cf3eb29d36da53c90c7ad19bc6c98
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-08 00:06:16 +0000

    Testing Sat Mar 08 2025 00:06:16 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex01/student/Sandwich.java
+++ chapter4/ex01/student/Sandwich.java
@@ -1,1 +1,41 @@
-// Write your code here
+import java.util.Scanner;
+
+public class Sandwich
+{
+    String mainIngredient;
+    String bread;
+    double price;
+
+    public Sandwich()
+    {
+        this.mainIngredient = "";
+        this.bread = "";
+        this.price = 0.0;
+    }
+
+    public String getMainIngredient()
+    {
+        return mainIngredient;
+    }
+    public void setMainIngredient(String mainIngredient)
+    {
+        this.mainIngredient = mainIngredient;
+    }
+    public String getBread()
+    {
+        return bread;
+    }
+    public void setBread(String bread)
+    {
+        this.bread = bread;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+}
--- chapter4/ex01/student/TestSandwich.java
+++ chapter4/ex01/student/TestSandwich.java
@@ -1,1 +1,28 @@
-// Write your code here
+import java.util.Scanner;
+
+public class TestSandwich
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the main sandwich ingredient >> ");
+        String mainIngredient = input.nextLine();
+
+        System.out.print("Enter bread type >> ");
+        String bread = input.nextLine();
+
+        System.out.print("Enter sandwich price >> ");
+        double price = input.nextDouble();
+
+        Sandwich sandwich = new Sandwich();
+        sandwich.setMainIngredient(mainIngredient);
+        sandwich.setBread(bread);
+        sandwich.setPrice(price);
+
+        System.out.print("You have ordered a " + sandwich.getMainIngredient() + " sandwich on " + 
+           sandwich.getBread() + " bread, and the price is " + sandwich.getPrice());
+
+        input.close();
+    }
+}
