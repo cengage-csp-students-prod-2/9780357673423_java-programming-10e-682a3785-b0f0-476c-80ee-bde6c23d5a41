a52d3b7b0d83e164cb2ee3807f6f8b2d47c53a0a
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-19 15:53:59 +0000

    Testing Wed Mar 19 2025 15:53:59 GMT+0000 (Coordinated Universal Time)

--- chapter6/de04/student/DebugSix4.java
+++ chapter6/de04/student/DebugSix4.java
@@ -6,14 +6,14 @@ public class DebugSix4
    public static void main(String[] args)
    {
       int high, low;
-      int howManyBetween;
+      double howManyBetween;
       double half, midPoint;
       Scanner input = new Scanner(System.in);
       System.out.print("Enter a number >> ");
-      low = inputnextInt();
+      low = input.nextInt();
       System.out.print("Enter a larger number >> ");
       high = input.nextInt();
-      while(low < high)
+      while(low > high)
       {
          System.out.println("The number you entered for a high number, " +
             high + ", is not more than " + low);
@@ -26,3 +26,4 @@ public class DebugSix4
       System.out.println(midPoint + " is halfway between " +
          low + " and " + high);
    }
+}
