5402e6b88c42d6993fadd5ccf2455680953424f7
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-20 23:14:42 +0000

    Testing Thu Mar 20 2025 23:14:42 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex05/student/Perfect.java
+++ chapter6/ex05/student/Perfect.java
@@ -1,1 +1,48 @@
-// Write your code here
+import java.util.Scanner;
+public class Perfect
+{
+    public static boolean isPerfect(int number)
+    {
+        int sum = 0;
+        for(int i = 1; i <= number / 2; i++)
+        {
+            if(number % i == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+    
+    public static void findPerfectNumbers(int maxNum)
+    {
+        boolean found = false;
+        for(int num = 1; num <= maxNum; num++)
+        {
+            if(isPerfect(num))
+            {
+                System.out.println("The number " + num + " is perfect");
+                found = true;
+            }
+        }
+        if(!found){
+            System.out.println("No perfect numbers found.");
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a maximum number >> ");
+        if(input.hasNextInt()){
+            int maxNum = input.nextInt();
+            if(maxNum >= 1){
+                findPerfectNumbers(maxNum);
+            } else {
+                System.out.println("Please enter a number greater than 0.");
+            }
+        } else {
+            System.out.println("Invalid input! Please enter a valid integer.");
+        }
+        input.close();
+    }
+}
