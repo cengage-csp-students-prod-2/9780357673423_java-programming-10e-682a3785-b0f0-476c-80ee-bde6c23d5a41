3e99cfef7305883a1c802aba92d935be3184e246
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-28 13:39:36 +0000

    Testing Fri Mar 28 2025 13:39:36 GMT+0000 (Coordinated Universal Time)

--- chapter7/de01/student/DebugSeven1.java
+++ chapter7/de01/student/DebugSeven1.java
@@ -9,19 +9,19 @@ public class DebugSeven1
       System.out.println("Enter three names.");
       System.out.println("They can be the same or different.");
       System.out.print("First name >> ");
-      name1 = kb.nextLine;
+      name1 = kb.nextLine();
       System.out.print("Second name >> ");
-      name2 = kb.nextLine;
+      name2 = kb.nextLine();
       System.out.print("Third name >> ");
-      name3 = kb.nextLine;
+      name3 = kb.nextLine();
       compareNames(name1, name2);
       compareNames(name1, name3);
       compareNames(name2, name3);
    }
-   public static void comparNames(String n1, n2)
+   public static void compareNames(String n1, String n2)
    {
       System.out.print(n1 + " and " + n2);
-      if(n1.equals(n2)
+      if(n1.equals(n2))
          System.out.println(" are the same");
       else
          System.out.println(" are different");
