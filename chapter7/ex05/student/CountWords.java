e37a020257b19ec95d03420e58d96a96e0dd5cf0
DJohnson77 <djohnson0936@kctcs.edu>
2025-03-29 13:42:08 +0000

    Testing Sat Mar 29 2025 13:42:08 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex05/student/CountWords.java
+++ chapter7/ex05/student/CountWords.java
@@ -1,1 +1,21 @@
-// Write your code here
+import java.util.*;
+public class CountWords
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        String quote = "";
+        
+        System.out.print("Enter a string >> ");
+        quote = kb.nextLine();
+
+        String cleanedQuote = quote.replaceAll("[^a-zA-Z0-9' ]", "");
+
+        String[] words = cleanedQuote.trim().split("\\s+");
+        int wordCount = words.length;
+
+        System.out.println("There are " + wordCount + " words in the string");
+
+        kb.close();
+    }
+}
