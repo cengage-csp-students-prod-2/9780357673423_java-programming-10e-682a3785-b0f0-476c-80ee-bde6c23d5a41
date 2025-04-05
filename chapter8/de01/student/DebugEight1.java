cf5b30d3be815b4d65502c355f69d6b896b08295
DJohnson77 <djohnson0936@kctcs.edu>
2025-04-05 13:20:54 +0000

    Testing Sat Apr 05 2025 13:20:54 GMT+0000 (Coordinated Universal Time)

--- chapter8/de01/student/DebugEight1.java
+++ chapter8/de01/student/DebugEight1.java
@@ -11,11 +11,11 @@ public class DebugEight1
       Scanner input = new Scanner(System.in); 
       char userCode;
       String entry;
-      boolean found = true;
+      boolean found = false;
       char[] okayCodes = {'A','C','T','H'};
       System.out.println("Enter shipping code for this delivery.");
       System.out.print("Valid codes are: ");
-      for(int x = 0; x < 2; ++x)
+      for(int x = 0; x < 4; ++x)
       {
           System.out.print(okayCodes[x]);
           if(x != (okayCodes.length - 1))
@@ -23,13 +23,12 @@ public class DebugEight1
       }
       System.out.print(" >> ");
       entry = input.nextLine(); 
-      userCode = entry.charAt(1);
+      userCode = entry.charAt(0);
       for(int i = 0; i < okayCodes.length; ++i)
       {
-         if(userCode = okayCodes[i])
-         {
+         if(userCode == okayCodes[i])
             found = true;
-         }
+            break;
       }
       if(!found)
          System.out.println("Good code");
