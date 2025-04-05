24d69b4c0a2a3e55914ee41742ef36021f4e0344
DJohnson77 <djohnson0936@kctcs.edu>
2025-04-05 21:37:50 +0000

    Testing Sat Apr 05 2025 21:37:50 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex04/student/ArrayMethodDemo.java
+++ chapter8/ex04/student/ArrayMethodDemo.java
@@ -3,29 +3,24 @@ import java.util.Scanner;
 public class ArrayMethodDemo {
 
     public static void main(String[] args) {
-        // Declare an array to store the numbers
         int[] numbers = new int[10];
-
-        // Scanner to take input from the user
         Scanner input = new Scanner(System.in);
 
-        // Input 10 integers
+        
         for (int i = 0; i < 10; i++) {
             System.out.print("Enter integer #" + (i + 1) + " >> ");
             numbers[i] = input.nextInt();
         }
 
-        // Display the numbers and their details
         display(numbers);
         displayReverse(numbers);
         displaySum(numbers);
-        displayLessThan(numbers, 12);  // Define the limit 12
+        displayLessThan(numbers, 12);  
         double average = calculateAverage(numbers);
         System.out.println("The average is " + average);
         displayHigherThanAverage(numbers, average);
     }
 
-    // Method to display all numbers
     public static void display(int[] numbers) {
         System.out.print("The numbers are ");
         for (int num : numbers) {
@@ -34,7 +29,6 @@ public class ArrayMethodDemo {
         System.out.println();
     }
 
-    // Method to display numbers in reverse order
     public static void displayReverse(int[] numbers) {
         System.out.print("The numbers in reverse order are ");
         for (int i = numbers.length - 1; i >= 0; i--) {
@@ -43,7 +37,6 @@ public class ArrayMethodDemo {
         System.out.println();
     }
 
-    // Method to display the sum of all numbers
     public static void displaySum(int[] numbers) {
         int sum = 0;
         for (int num : numbers) {
@@ -52,7 +45,6 @@ public class ArrayMethodDemo {
         System.out.println("The sum of all numbers is " + sum);
     }
 
-    // Method to display numbers less than the given limit
     public static void displayLessThan(int[] numbers, int limit) {
         System.out.print("The numbers less than the limit " + limit + " are ");
         boolean found = false;
@@ -68,7 +60,6 @@ public class ArrayMethodDemo {
         System.out.println();
     }
 
-    // Method to display numbers greater than the average
     public static void displayHigherThanAverage(int[] numbers, double average) {
         System.out.print("The numbers greater than the average " + average + " are ");
         boolean found = false;
@@ -84,7 +75,6 @@ public class ArrayMethodDemo {
         System.out.println();
     }
 
-    // Method to calculate the average
     public static double calculateAverage(int[] numbers) {
         int sum = 0;
         for (int num : numbers) {
