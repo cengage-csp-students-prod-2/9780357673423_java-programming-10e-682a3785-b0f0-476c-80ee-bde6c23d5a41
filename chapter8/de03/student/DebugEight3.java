0d6c1bd373557d04d0d5a7d41b0b8d876a1431ed
DJohnson77 <djohnson0936@kctcs.edu>
2025-04-05 14:20:09 +0000

    Testing Sat Apr 05 2025 14:20:09 GMT+0000 (Coordinated Universal Time)

--- chapter8/de03/student/DebugEight3.java
+++ chapter8/de03/student/DebugEight3.java
@@ -6,12 +6,12 @@ public class DebugEight3
 {
    public static void main(String[] args)
    { 
-      Scanner input = Scanner(System.in); 
+      Scanner input = new Scanner(System.in); 
       String entry;
       char[] floorPlans = {'A','B','C','a','b','c'};
       int[] pricesInThousands = {145, 190, 235};
-      char plan;
-      int x, fp = 99;
+      char plan = ' ';
+      int x, fp = -1;
       System.out.println("Please select a floor plan");
       System.out.println("Our floor plans are:");
       System.out.println("   A - Augusta, a ranch");
@@ -19,14 +19,23 @@ public class DebugEight3
       System.out.println("   C - Colonial, a two-story");
       System.out.print("Enter floor plan letter >> ");
       entry = input.nextLine();
-      for(x = 0; x < floorPlans.length; ++x);
-	 if(plan == floorPlans[x]);
-           fp = x;
-      if(fp == 99)
+      if(entry.length() > 0)
+         plan = entry.charAt(0);
+      else{
+         System.out.println("No input entered");
+      }
+
+      for(x = 0; x < floorPlans.length; ++x){
+	      if(plan == floorPlans[x]){
+            fp = x;
+            break;
+         }
+      }
+      if(fp == -1)
         System.out.println("Invalid floor plan code entered");
       else
       {
-        if(fp > floorPlans.length - 1);
+        if(fp > floorPlans.length - 1)
            fp = fp - floorPlans.length;
         System.out.println("Model " + plan +
            " is priced at only $" +
