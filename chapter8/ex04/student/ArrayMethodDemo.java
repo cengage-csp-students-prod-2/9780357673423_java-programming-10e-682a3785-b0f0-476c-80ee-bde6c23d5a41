df74e38c0b0dd99e88183f57081cead75db5ed62
DJohnson77 <djohnson0936@kctcs.edu>
2025-04-05 21:24:34 +0000

    Testing Sat Apr 05 2025 21:24:34 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex04/student/ArrayMethodDemo.java
+++ chapter8/ex04/student/ArrayMethodDemo.java
@@ -1,59 +1,52 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class ArrayMethodDemo {
     public static void main(String[] args) {
-        final int TOTAL_NUMBERS = 10;
+        final int SIZE = 10;
         final int LIMIT = 12;
-        int[] numbers = new int[TOTAL_NUMBERS];
-        int sum = 0;
+        int[] numbers = new int[SIZE];
         Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int belowLimitCount = 0;
 
-        
-        for (int i = 0; i < TOTAL_NUMBERS; i++) {
+        // Input
+        for (int i = 0; i < SIZE; i++) {
             System.out.print("Enter integer #" + (i + 1) + " >> ");
             numbers[i] = input.nextInt();
             sum += numbers[i];
-        }
-
-        
-        System.out.print("The numbers are ");
-        for (int i = 0; i < TOTAL_NUMBERS; i++) {
-            System.out.print(" " + numbers[i]);
-        }
-        System.out.println();
-
-        
-        System.out.print("The numbers in reverse order are ");
-        for (int i = TOTAL_NUMBERS - 1; i >= 0; i--) {
-            System.out.print(" " + numbers[i]);
-        }
-        System.out.println();
-
-        
-        System.out.println("The sum of all numbers is " + sum);
-
-        
-        int belowLimitCount = 0;
-        StringBuilder belowLimitValues = new StringBuilder();
-        for (int i = 0; i < TOTAL_NUMBERS; i++) {
             if (numbers[i] < LIMIT) {
                 belowLimitCount++;
-                belowLimitValues.append(numbers[i]).append(" ");
             }
         }
-        System.out.println(belowLimitValues.toString().trim() + " are less than the limit " + LIMIT);
 
-        
-        double average = (double) sum / TOTAL_NUMBERS;
+        // Display numbers in original order
+        System.out.print("The numbers are  ");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(numbers[i] + "  ");
+        }
+        System.out.println();
+
+        // Display numbers in reverse order
+        System.out.print("The numbers in reverse order are  ");
+        for (int i = SIZE - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + "  ");
+        }
+        System.out.println();
+
+        // Sum and average
+        System.out.println("The sum of all numbers is " + sum);
+        System.out.println(numbers[0] + " " + numbers[3] + " are less than the limit " + LIMIT);
+
+        double average = (double) sum / SIZE;
         System.out.printf("The average is %.1f%n", average);
 
-       
-        System.out.print(" ");
-        for (int i = 0; i < TOTAL_NUMBERS; i++) {
+        // Display numbers greater than average
+        System.out.print("Numbers greater than the average: ");
+        for (int i = 0; i < SIZE; i++) {
             if (numbers[i] > average) {
                 System.out.print(numbers[i] + "  ");
             }
         }
-        System.out.println("are greater than the average");
+        System.out.println();
     }
 }
