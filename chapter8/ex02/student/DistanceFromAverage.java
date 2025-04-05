34dd3bfc90217bb676f9cf5b73a4df8a76cc6467
DJohnson77 <djohnson0936@kctcs.edu>
2025-04-05 21:00:19 +0000

    Testing Sat Apr 05 2025 21:00:19 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex02/student/DistanceFromAverage.java
+++ chapter8/ex02/student/DistanceFromAverage.java
@@ -1,1 +1,36 @@
-// Write your code here
+import java.util.*;
+public class DistanceFromAverage{
+    public static void main(String[] args){
+        final int MAX = 15;
+        final int QUIT = 99999;
+        double[] distances = new double[MAX];
+        int count = 0;
+        double sum = 0;
+        
+        Scanner input = new Scanner(System.in);
+        double entry;
+
+        System.out.print("Enter a numeric value or 99999 to quit >> ");
+        entry = input.nextDouble();
+
+        while(entry != QUIT && count < MAX){
+            distances[count] = entry;
+            sum += entry;
+            count++;
+            System.out.print("Enter next numeric value or 99999 to quit >> ");
+            entry = input.nextDouble();
+        }
+
+        if(count == 0){
+            System.out.println("You must enter a numeric value.");
+        }else{
+            double average = sum / count;
+            System.out.printf("You entered %d numbers and their average is %.3f\n", count, average);
+
+            for(int i = 0; i < count; i++){
+                double difference = distances[i] - average;
+                System.out.println(distances[i] + " is " + difference + " away from the average");
+            }
+        }
+    }
+}
